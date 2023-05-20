554af0cb0191b5b7b55b89c4614e95fcb433096a
Andrei Tecsor <andrei.tecsor@gmail.com>
2023-05-20 20:44:25 +0300

    send encrypted data and server key to provider api's tcp client . close #7

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-mod keys_manager;
-
-#[allow(unused_imports)]
-
-use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
+#[allow(unused_imports)]
+use std::net::{TcpListener, TcpStream};
+use bincode::serialized_size;
+
 use tfhe::{ConfigBuilder, FheUint8, generate_keys};
 use tfhe::prelude::*;
+
 use crate::keys_manager::key_gen;
 
+mod keys_manager;
 
 #[derive(Debug)]
 enum FHEOperationType {
@@ -28,13 +29,6 @@ fn main() -> std::io::Result<()> {
 }
 
 fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
-    // tfhe-rs for generating keys
-    // Generate tfhe-rs client_key and server_key
-    let (mut client_key, mut server_key) = key_gen().map_err(|e| {
-        std::io::Error::new(std::io::ErrorKind::Other, e.to_string())
-    })?;
-
-
     // prepare a buffer to hold the data
     let mut buffer = [0u8; 4];
 
@@ -55,32 +49,53 @@ fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
     // print the operation type
     println!("Received operation: {:?}", operation);
 
-     match operation {
-            FHEOperationType::Encrypt => {
-                // read another int value
+    match operation {
+        FHEOperationType::Encrypt => {
+            // read another int value
+            stream.read_exact(&mut buffer)?;
+            let num_values = i32::from_le_bytes(buffer);
+
+            println!("Number of subsequent values: {}", num_values);
+
+            let mut values_to_encrypt = vec![0u8; num_values as usize];
+            // read each subsequent value
+            for _ in 0..num_values {
                 stream.read_exact(&mut buffer)?;
-                let num_values = i32::from_le_bytes(buffer);
-
-                println!("Number of subsequent values: {}", num_values);
-
-                // read each subsequent value
-                for _ in 0..num_values {
-                    stream.read_exact(&mut buffer)?;
-                    let value = i32::from_le_bytes(buffer);
-                    println!("Received value: {}", value);
-                    //Encrypt values using client_key
-                    let ciphertext = FheUint8::encrypt(value as u8, &client_key);
-                }
+                let value = i32::from_le_bytes(buffer);
+                println!("Received value: {}", value);
+                //Save the values in an array
+                values_to_encrypt.push(value as u8);
             }
-            FHEOperationType::Decrypt => {
-                // do something for decryption
-                println!("This is a Decrypt operation");
+
+            // send a response letting the client know that
+            let response = "Hello, client! Your data is now being prepared, please wait...";
+            stream.write_all(response.as_bytes())?;
+
+            // Generate tfhe-rs client_key and server_key
+            let (mut client_key, mut server_key) = key_gen().map_err(|e| {
+                std::io::Error::new(std::io::ErrorKind::Other, e.to_string())
+            })?;
+
+            //send encrypted values to the client (one by one)
+            println!("Sending encrypted values to client");
+            for value in values_to_encrypt {
+                println!("Sending value: {}", value);
+                let encrypted_value = FheUint8::encrypt(value, &mut client_key);
+                bincode::serialize_into(&mut stream, &encrypted_value).unwrap();
             }
+
+            //send the server_key to the client
+            println!("Sending server key to client");
+            bincode::serialize_into(&mut stream, &server_key).unwrap();
+
         }
+        FHEOperationType::Decrypt => {
+            // do something for decryption
+            println!("This is a Decrypt operation");
+        }
+    }
 
-    // send a response back to the client
-    let response = "Hello, client!";
-    stream.write_all(response.as_bytes())?;
+
     Ok(())
 }
 
