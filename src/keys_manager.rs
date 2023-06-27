d6f841ea5a8f70f37d1d0c1f889e229752759618
Andrei Tecsor <andrei.tecsor@gmail.com>
2023-06-27 20:04:16 +0300

    finishing encrypt phase

--- src/keys_manager.rs
+++ src/keys_manager.rs
@@ -2,49 +2,59 @@ use std::error::Error;
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
 use std::path::Path;
-use tfhe::{ClientKey, ConfigBuilder, ServerKey};
+
+use tfhe::integer::{gen_keys_radix, RadixClientKey, ServerKey};
+use tfhe::shortint::parameters::PARAM_MESSAGE_2_CARRY_2;
+
+// Import the RadixClientKey and ServerKey types from the appropriate crate.
+// The import path may vary depending on your actual codebase.
 
 const CLIENT_KEY_FILE_PATH: &'static str = "client_key.bin";
 const SERVER_KEY_FILE_PATH: &'static str = "server_key.bin";
 
-pub fn key_gen() -> Result<(ClientKey, ServerKey), Box<dyn Error>> {
+pub(crate) fn key_gen(num_block: u32) -> Result<(RadixClientKey, ServerKey), Box<dyn Error + Send + Sync>> {
     let client_key_path = Path::new(CLIENT_KEY_FILE_PATH);
-
-    let client_keys: ClientKey = if client_key_path.exists() {
-        println!("Reading client keys from {}", CLIENT_KEY_FILE_PATH);
-        let mut file = BufReader::new(File::open(client_key_path)?);
-        bincode::deserialize_from(file)?
-    } else {
-        println!(
-            "No {} found, generating new keys and saving them",
-            CLIENT_KEY_FILE_PATH
-        );
-        let config = ConfigBuilder::all_disabled()
-            .enable_default_uint8()
-            .build();
-        let k = ClientKey::generate(config);
-        let file = BufWriter::new(File::create(client_key_path)?);
-        bincode::serialize_into(file, &k)?;
-
-        k
-    };
-
     let server_key_path = Path::new(SERVER_KEY_FILE_PATH);
-    let server_keys: ServerKey = if server_key_path.exists() {
-        println!("Reading server keys from {}", CLIENT_KEY_FILE_PATH);
-        let mut file = BufReader::new(File::open(server_key_path)?);
-        bincode::deserialize_from(file).unwrap()
+
+    if client_key_path.exists() && server_key_path.exists() {
+        let client_key: RadixClientKey = {
+            let file = File::open(client_key_path)
+                .map_err(|e| Box::new(e) as Box<dyn Error + Send + Sync>)?;
+            let reader = BufReader::new(file);
+            bincode::deserialize_from(reader)
+                .map_err(|e| Box::new(e) as Box<dyn Error + Send + Sync>)?
+        };
+
+        let server_key: ServerKey = {
+            let file = File::open(server_key_path)
+                .map_err(|e| Box::new(e) as Box<dyn Error + Send + Sync>)?;
+            let reader = BufReader::new(file);
+            bincode::deserialize_from(reader)
+                .map_err(|e| Box::new(e) as Box<dyn Error + Send + Sync>)?
+        };
+
+        Ok((client_key, server_key))
     } else {
-        println!(
-            "No {} found, generating new keys and saving them",
-            SERVER_KEY_FILE_PATH
-        );
-        let k = client_keys.generate_server_key();
-        let file = BufWriter::new(File::create(server_key_path)?);
-        bincode::serialize_into(file, &k).unwrap();
+        let now = std::time::Instant::now();
+        let (client_key, server_key) = gen_keys_radix(&PARAM_MESSAGE_2_CARRY_2, num_block as usize);
+        println!("Key generation: {}ms", now.elapsed().as_millis());
 
-        k
-    };
+        {
+            let client_file = File::create(client_key_path)
+                .map_err(|e| Box::new(e) as Box<dyn Error + Send + Sync>)?;
+            let writer = BufWriter::new(client_file);
+            bincode::serialize_into(writer, &client_key)
+                .map_err(|e| Box::new(e) as Box<dyn Error + Send + Sync>)?;
+        }
 
-    Ok((client_keys, server_keys))
+        {
+            let server_file = File::create(server_key_path)
+                .map_err(|e| Box::new(e) as Box<dyn Error + Send + Sync>)?;
+            let writer = BufWriter::new(server_file);
+            bincode::serialize_into(writer, &server_key)
+                .map_err(|e| Box::new(e) as Box<dyn Error + Send + Sync>)?;
+        }
+
+        Ok((client_key, server_key))
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 use std::io::{Read, Write};
 #[allow(unused_imports)]
 use std::net::{TcpListener, TcpStream};
-use bincode::serialized_size;
-
-use tfhe::{ConfigBuilder, FheUint8, generate_keys};
-use tfhe::prelude::*;
 
 use crate::keys_manager::key_gen;
 
@@ -52,41 +48,71 @@ fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
     match operation {
         FHEOperationType::Encrypt => {
             // read another int value
-            stream.read_exact(&mut buffer)?;
-            let num_values = i32::from_le_bytes(buffer);
-
-            println!("Number of subsequent values: {}", num_values);
-
-            let mut values_to_encrypt = vec![0u8; num_values as usize];
-            // read each subsequent value
-            for _ in 0..num_values {
-                stream.read_exact(&mut buffer)?;
-                let value = i32::from_le_bytes(buffer);
-                println!("Received value: {}", value);
-                //Save the values in an array
-                values_to_encrypt.push(value as u8);
-            }
 
             // send a response letting the client know that
             let response = "Hello, client! Your data is now being prepared, please wait...";
             stream.write_all(response.as_bytes())?;
 
+            let num_block = 8; // 4*2 = 8 bits, 8*2 = 16 bits
+            let (client_key, server_key) = key_gen(num_block).map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+
+            stream.read_exact(&mut buffer)?;
+            let gambling_percent = i32::from_le_bytes(buffer);
+            println!("gambling_percent: {}", gambling_percent);
+
+            stream.read_exact(&mut buffer)?;
+            let overspending_score = i32::from_le_bytes(buffer);
+            println!("overspending_score: {}", overspending_score);
+
+            stream.read_exact(&mut buffer)?;
+            let impulsive_buying_score = i32::from_le_bytes(buffer);
+            println!("impulsive_buying_score: {}", impulsive_buying_score);
+
+            stream.read_exact(&mut buffer)?;
+            let mean_deposit_sum = i32::from_le_bytes(buffer);
+            println!("mean_deposit_sum: {}", mean_deposit_sum);
+
+            stream.read_exact(&mut buffer)?;
+            let mean_reported_income = i32::from_le_bytes(buffer);
+            println!("mean_reported_income: {}", mean_reported_income);
+
+            stream.read_exact(&mut buffer)?;
+            let no_months_deposited = i32::from_le_bytes(buffer);
+            println!("no_months_deposited: {}", no_months_deposited);
+
             // Generate tfhe-rs client_key and server_key
-            let (mut client_key, mut server_key) = key_gen().map_err(|e| {
-                std::io::Error::new(std::io::ErrorKind::Other, e.to_string())
-            })?;
+            let encrypted_gambling_percent = client_key.encrypt(gambling_percent as u64);
+            let encrypted_overspending_score = client_key.encrypt(overspending_score as u64);
+            let encrypted_impulsive_buying_score = client_key.encrypt(impulsive_buying_score as u64);
+            let encrypted_mean_deposit_sum = client_key.encrypt(mean_deposit_sum as u64);
+            let encrypted_mean_reported_income = client_key.encrypt(mean_reported_income as u64);
+            let encrypted_no_months_deposited = client_key.encrypt(no_months_deposited as u64);
 
             //send encrypted values to the client (one by one)
             println!("Sending encrypted values to client");
-            for value in values_to_encrypt {
-                println!("Sending value: {}", value);
-                let encrypted_value = FheUint8::encrypt(value, &mut client_key);
-                bincode::serialize_into(&mut stream, &encrypted_value).unwrap();
-            }
 
-            //send the server_key to the client
-            println!("Sending server key to client");
-            bincode::serialize_into(&mut stream, &server_key).unwrap();
+
+            // The key size is 109518176 bytes => in order to optimise performance, we will share the key
+            // with the requester only once, at the beginning of the connection
+
+                // let server_key_size = bincode::serialized_size(&server_key)
+                //     .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
+                // println!("Server key serialized size: {}", server_key_size);
+                // bincode::serialize_into(&mut stream, &server_key_size).unwrap();
+                // bincode::serialize_into(&mut stream, &server_key).unwrap();
+
+            let bytes_size = bincode::serialized_size(&encrypted_gambling_percent)
+                .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
+            println!("Gambling percent serialized size: {}", bytes_size);
+            let gambling_percent_size_bytes = bytes_size.to_be_bytes();
+            stream.write_all(&gambling_percent_size_bytes)?; //all the sizes are the same, so we can send only one
+
+            bincode::serialize_into(&mut stream, &encrypted_gambling_percent).unwrap();
+            bincode::serialize_into(&mut stream, &encrypted_overspending_score).unwrap();
+            bincode::serialize_into(&mut stream, &encrypted_impulsive_buying_score).unwrap();
+            bincode::serialize_into(&mut stream, &encrypted_mean_deposit_sum).unwrap();
+            bincode::serialize_into(&mut stream, &encrypted_mean_reported_income).unwrap();
+            bincode::serialize_into(&mut stream, &encrypted_no_months_deposited).unwrap();
 
         }
         FHEOperationType::Decrypt => {
@@ -94,7 +120,6 @@ fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
             println!("This is a Decrypt operation");
         }
     }
-
 
     Ok(())
 }
